a604c728e5234eed14ea6f67c52e105c945b146a
kmcguire <kmcg3413@gmail.com>
2014-12-22 10:52:57 -0600

    working on tcp bridges and new design for making it safer to write the tcp bridge object to support cloning and still be safe to work with

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,29 +1,75 @@
 use std::sync::Arc;
 use std::sync::Mutex;
+use std::intrinsics::transmute;
 
 use net::Net;
 use net::NetProtocolAddress;
 
-pub type TCPLISTENER = Arc<Mutex<TCPListener>>;
-pub type TCPCONNECTOR = Arc<Mutex<TCPConnector>>;
+pub type TCPLISTENER = Arc<RacyCell<TCPListener>>;
+pub type TCPCONNECTOR = Arc<RacyCell<TCPConnector>>;
 
-pub struct TCPListener {
-    a: uint,
-}
-pub struct TCPConnector {
-    a: uint,
+pub struct RacyCell<T> {
+    t:      T
 }
 
-impl Sync for TCPListener { }
-
-impl TCPListener {
-    pub fn new(net: &Net, host: String, port: u16) -> TCPLISTENER {
-        Arc::new(Mutex::new(TCPListener { a: 0 }))
+impl<T> Deref<T> for RacyCell<T> {
+    fn deref<'a>(&'a self) -> &'a T {
+        &self.t
     }
 }
 
+// Trying a new design here. It is dangerous to have to maintain
+// a seperate inner structure just to gain clone functionality.
+// Since Arc provides it we can use it, but Arc restricts the deref
+// to immutable. So this is my workaround to being able to use Arc.
+impl<T> RacyCell<T> {
+    pub fn new(t: T) -> RacyCell<T> {
+        RacyCell {t: t}
+    }
+
+    // This breaks mutability/immutability because it lets you take
+    // anything that is immutable and produce an mutable reference.
+    fn mutref(&self) -> &mut T {
+        let p: &mut RacyCell<T>;
+
+        p = unsafe { transmute(self) };
+
+        &mut p.t
+    }
+}
+
+pub struct TCPListener {
+    net:    Net,
+}
+pub struct TCPConnector {
+    net:    Net,
+}
+
+impl TCPListener {
+    pub fn thread(s: TCPLISTENER) {
+
+    }
+
+    pub fn new(net: &Net, host: String, port: u16) -> TCPLISTENER {
+        let n = Arc::new(RacyCell::new(TCPListener { net: net.clone() }));
+        let nclone = n.clone();
+        spawn(move || { TCPListener::thread(nclone) });
+
+        n
+    }
+
+}
+
 impl TCPConnector {
+    pub fn thread(s: TCPCONNECTOR) {
+
+    }
+
     pub fn new(net: &Net, host: String, port: u16) -> TCPCONNECTOR {
-        Arc::new(Mutex::new(TCPConnector { a: 0 }))
+        let n = Arc::new(RacyCell::new(TCPConnector { net: net.clone() }));
+        let nclone = n.clone();
+        spawn(move || { TCPConnector::thread(nclone)});
+
+        n
     }
 }
